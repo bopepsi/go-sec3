5c9fff376abaa3f95dbaded4db570c406678725e
bopepsi <bopepsi@gmail.com>
2022-09-15 15:07:15 -0400

    add-middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,13 +6,19 @@ import (
 	"github.com/bopepsi/go-app/pkg/config"
 	"github.com/bopepsi/go-app/pkg/handlers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := chi.NewRouter()
+	router := chi.NewRouter()
 
-	mux.Get("/", handlers.Repo.Home)
-	mux.Get("/about", handlers.Repo.About)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Logger)
+	router.Use(WriteToConsole)
+	router.Use(NoSurf)
 
-	return mux
+	router.Get("/", handlers.Repo.Home)
+	router.Get("/about", handlers.Repo.About)
+
+	return router
 }
